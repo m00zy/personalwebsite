184ad3550591360bed2da4dcb37754c9b4d61ab6
m00zy <maxwellzhu123@gmail.com>
2024-08-16 16:35:29 +1200

    add NavBar component to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Bio from './components/Bio';
+import NavBar from './components/NavBar';
 import './Home.css';
 
 function Home() {
     return (
         <div className='home'>
             <Bio />
+            <NavBar />
         </div>
     );
 }
---
+++ src/pages/Home/components/NavBar.jsx
@@ -0,0 +1,7 @@
+function NavBar() {
+    return (
+        <p>Navbar</p>
+    );
+}
+
+export default NavBar;
