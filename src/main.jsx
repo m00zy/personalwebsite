206964e816d7ec85fe90e359cd45c08e2b3e8498
m00zy <maxwellzhu123@gmail.com>
2024-08-20 13:19:49 +1200

    add root eleemnt to main.jxs

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom';
+import { Root } from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
