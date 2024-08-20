4df6602a209bb1339ab4f1987b6fd63d9188347a
m00zy <maxwellzhu123@gmail.com>
2024-08-20 16:06:36 +1200

    improve the content and styling of footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,11 @@ import './footer.css'
 function Footer () {
     return (
         <div className='footer'>
-            <p>maxwellzhudev@gmail.com</p>
+            <div className='contacts'>
+                <p className='contact'>Business: maxwellzhudev@gmail.com</p>
+                <p className='contact'>Personal: maxwellzhu123@gmail.com</p>
+                <p className='contact'>(+64) 223149243</p>
+            </div>
         </div>
     )
 }
