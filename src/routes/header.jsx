2eb41851bf08e0c8886bdd5df0adbc28c62ed6b8
m00zy <maxwellzhu123@gmail.com>
2024-08-20 13:45:06 +1200

    add content and styling to header

--- src/routes/header.jsx
+++ src/routes/header.jsx
@@ -1,6 +1,18 @@
+import './header.css';
+
 function Header() {
     return (
-        <p>Hello World!</p>
+        <div className='header'>
+            <div className='nav-container'>
+                <nav className='nav'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Projects</p>
+                    <p>Resume</p>
+                    <p>Other</p>
+                </nav>
+            </div>
+        </div>
     );
 }
 
