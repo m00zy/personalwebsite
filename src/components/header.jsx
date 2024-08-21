1ec73bd490ecf63e481aa9d5f1303e8356e0fad1
m00zy <maxwellzhu123@gmail.com>
2024-08-21 17:07:34 +1200

    imrprove header styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className='header'>
-            <div className='nav-container'>
-                <nav className='nav'>
-                    <Link to={``}>Home</Link>
-                    {/* <Link to={`about`}>About</Link>
-                    <Link to={`projects`}>Projects</Link> */}
-                    <a href={resume} target="_blank" rel="noreferrer">Resume</a>
-                </nav>
+        <div className='header-container'>
+            <div className='header'>
+                <div className='nav-container'>
+                    <nav className='nav'>
+                        <Link to={``}>Home</Link>
+                        {/* <Link to={`about`}>About</Link>
+                        <Link to={`projects`}>Projects</Link> */}
+                        <a href={resume} target="_blank" rel="noreferrer">Resume</a>
+                    </nav>
+                </div>
             </div>
         </div>
     );
