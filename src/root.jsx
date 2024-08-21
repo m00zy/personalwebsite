48eeae06db716b5f7df0acb181d0d1ae8bfaf20e
m00zy <maxwellzhu123@gmail.com>
2024-08-21 16:16:17 +1200

    implement max-width restriction on root

--- src/root.jsx
+++ src/root.jsx
@@ -6,14 +6,14 @@ import './root.css';
 
 function Root() {
     return (
-        <>
+        <div className='root'>
             <Header />
             <div className='body'>
                 <Outlet />
             </div>
             
             <Footer />
-        </>
+        </div>
     );
 }
 
