912b22cffbca441859cb084cc2499d5312d8281b
m00zy <maxwellzhu123@gmail.com>
2024-09-04 00:01:59 +1200

    add transition animations to linkedin and github icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,10 +7,12 @@ function Footer () {
                 <p>Business: <a className='hover:underline' href='mailto:maxwellzhudev@gmail.com'>maxwellzhudev@gmail.com</a> </p>
                 <p className='contact'>Personal: <a className='hover:underline' href='mailto:maxwellzhu123@gmail.com'>maxwellzhu123@gmail.com</a> </p>
                 <div className="text-2xl my-2">
-                    <a href="https://www.linkedin.com/in/maxwell-zhu-1a292b142/" target="_blank" className="hover:text-blue-900">
+                    <a href="https://www.linkedin.com/in/maxwell-zhu-1a292b142/" target="_blank" 
+                        className="transition-all delay-75 ease-in-out hover:-translate-y-1 hover:text-blue-900 hover:text-3xl duration-400">
                         <i className="fab fa-linkedin"></i>
                     </a>
-                    <a href="https://github.com/m00zy" target="_blank" className="mx-3">
+                    <a href="https://github.com/m00zy" target="_blank" 
+                    className="mx-3 transition-all delay-75 ease-in-out hover:-translate-y-1 hover:text-3xl duration-400">
                         <i className="fab fa-github"></i>
                     </a>
                 </div>
