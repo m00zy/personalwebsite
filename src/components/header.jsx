e6dcebe0e1e04b7a041a0560807afb453c519480
m00zy <maxwellzhu123@gmail.com>
2024-09-03 19:26:20 +1200

    transition home css styling to tailwind

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,12 @@
-// import './header.css';
 import resume from '../assets/Resume.pdf';
 import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className='w-full h-40 flex items-center justify-end'>
-                    <nav className='w-32 flex justify-between font-mono text-lg flex-shrink'>
+        <div className='w-full h-20 flex items-center justify-end'>
+                    <nav className='w-36 flex justify-between font-mono text-xl'>
                         <Link to={``}>Home</Link>
-                        {/* <Link to={`about`}>About</Link>
-                        <Link to={`projects`}>Projects</Link> */}
+
                         <a href={resume} target="_blank" rel="noreferrer">Resume</a>
                     </nav>
         </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,15 @@
-import './Home.css';
 import myImage from '../assets/me.JPG';
 
 function Body() {
     return (
-        <div className='body'>
-            <div className='welcome'>
-                <p className='first-line'>Hi, my name is MAXWELL</p>
-                <p className='second-line'>
+        <div className='h-2/3 flex flex-row justify-around'>
+            <div className='w-1/3 h-2/3 flex flex-col justify-evenly items-end my-10'>
+                <p className='text-5xl font-mono text-right'>Hi, my name is MAXWELL</p>
+                <p className='w-3/4 font-mono text-xl text-right'>
                     I'm an aspiring software developer, with a background in Computer Science, Bioengineering and Psychology
                 </p>
             </div>
-            <img className='myImage' src={myImage} 
+            <img className='w-1/2 object-contain' src={myImage} 
             alt='Individual standing calmly in front of a mountain, with the natural landscape in the background.'/>
         </div>
     );
