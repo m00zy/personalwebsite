73d1362da22397a72445423dba6c401252a119b8
m00zy <maxwellzhu123@gmail.com>
2025-12-27 17:45:39 +1300

    Update home description

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,7 @@ function Body() {
             <div className='w-1/3 h-2/3 flex flex-col justify-evenly items-end my-10'>
                 <p className='text-5xl font-mono text-right'>Hi, my name is MAXWELL</p>
                 <p className='w-3/4 font-mono text-xl text-right'>
-                    I'm a junior software developer, with a interdisciplinary background in Computer Science, Bioengineering and Psychology
+                    I'm a junior software engineer, with a interdisciplinary background in Computer Science, Bioengineering and Psychology
                 </p>
             </div>
             <div className='w-1/2'>
